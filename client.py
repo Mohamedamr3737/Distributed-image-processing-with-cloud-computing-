75a6d528591f80bda2cb892b8fd6436888d37191
Mohamedamr3737 <mohamedamr2002@hotmail.com>
2024-04-24 15:12:13 +0200

    segmentation but with combination problem

--- client.py
+++ client.py
@@ -15,11 +15,13 @@ def split_image(num_segments, image_bytes):
         if i == num_segments - 1:
             end = height
         segment = img[start:end, :, :]
-        # plt.imshow(segment, cmap='gray')
-        # plt.axis('off')
-        # plt.title("Segment number " + str(i+1))
-        # plt.show()
-        segments.append(segment)
+        
+        # Convert segment to bytes
+        segment_bytes = io.BytesIO()
+        Image.fromarray(segment).save(segment_bytes, format='JPEG')
+        segment_bytes.seek(0)
+        
+        segments.append(segment_bytes.read())
     return segments
 
 def display_image_from_bytes(image_bytes):
@@ -88,28 +90,49 @@ with open(image_path, 'rb') as f:
 #     client_socket.send("ed".encode('utf-8'))
 #     send_image_segments(client_socket,s)
 
-while True:
-
-    user_input = input("Enter the operation: ")
-    client_socket.send(user_input.encode('utf-8'))
-    send_image(client_socket, image_path)
-    image=receive_image(client_socket)
-    display_image_from_bytes(image[0])
-    user_input = input("do you want to quit Y/N: ")
-    if user_input=="Y":
-        client_socket.send("q".encode('utf-8'))
-        break
-    else:continue
-
-
-
 # while True:
-#     image=receive_image(client_socket)
-#     if image is None:
-#         continue
-#     else:
-#         display_image_from_bytes(image[0])
 
+#     user_input = input("Enter the operation: ")
+#     client_socket.send(user_input.encode('utf-8'))
+#     # send_image(client_socket, image_path)
+#     send_image_segments(client_socket,image_bytes)
+#     image=receive_image(client_socket)                                            
+#     display_image_from_bytes(image[0])
+#     user_input = input("do you want to quit Y/N: ")
+#     if user_input=="Y":
+#         client_socket.send("q".encode('utf-8'))
+#         break
+#     else:continue
+
+
+
+    # Send the image
+    #######################################################
+# processed_image_bytes = b''
+# segments=split_image(4,image_bytes)
+# for s in segments:
+#     client_socket.send("ed".encode('utf-8'))
+#     send_image_segments(client_socket, s)
+#     processed_segment_bytes= receive_image(client_socket)[0]
+#     processed_image_bytes = processed_image_bytes+processed_segment_bytes    
+#     # display_image_from_bytes(processed_segment_bytes)       
+#     display_image_from_bytes(processed_image_bytes)                        
+# display_image_from_bytes(processed_image_bytes)
+######################################
+# Send the image
+segments = split_image(2, image_bytes)
+processed_segments_bytes = []
+for segment in segments:
+    client_socket.send("ed".encode('utf-8'))
+    send_image_segments(client_socket, segment)
+    processed_segment_bytes, _ = receive_image(client_socket)
+    display_image_from_bytes(processed_segment_bytes)
+    processed_segments_bytes.append(processed_segment_bytes)
+# Concatenate all processed segments to form the processed image
+processed_image_bytes = b"".join(processed_segments_bytes)
+
+# Display the processed image
+display_image_from_bytes(processed_image_bytes)
 
 
 # Close the connection with the server
