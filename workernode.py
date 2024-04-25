a89d246d98db2f8b28b3819a5de3233516e325b1
Mohamedamr3737 <mohamedamr2002@hotmail.com>
2024-04-25 13:44:57 +0200

    functions removed from nodes and added to middleware-gitignore file added

--- workernode.py
+++ workernode.py
@@ -1,41 +1,8 @@
 import socket
 import threading
-import os
-import io
-from PIL import Image
 import cv2
 import numpy as np
-def send_image_knownbytes(conn, image):
-    image_bytes = image[1] 
-    # print(image_bytes)
-    # Send the length of the image first
-    conn.sendall(image_bytes.to_bytes(4, byteorder='big'))
-    # Send the image bytes
-    conn.sendall(image[0])
-
-
-def display_image_from_bytes(image_bytes):
-    # Create a BytesIO object to read the image bytes
-    image_stream = io.BytesIO(image_bytes)
-    # Open the image using PIL (Python Imaging Library)
-    image = Image.open(image_stream)
-    # Display the image
-    image.show()
-
-
-def receive_image(conn):
-    # Receive the length of the image
-    length = int.from_bytes(conn.recv(4), byteorder='big')
-    if length !=0:
-        # Receive the image bytes
-        image_bytes = b''
-        while len(image_bytes) < length:
-            data = conn.recv(length - len(image_bytes))
-            if not data:
-                break
-            image_bytes += data
-        return image_bytes,length
-
+from imageFunctionsMiddleware import *
 
 
 def handle_client(client_socket, addr):
