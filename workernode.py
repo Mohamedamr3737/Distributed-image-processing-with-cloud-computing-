c14238591386763602f7da3f248e38f6c038a1b2
Mohamedamr3737 <mohamedamr2002@hotmail.com>
2024-04-27 14:25:12 +0200

    image processing module added

--- workernode.py
+++ workernode.py
@@ -1,9 +1,7 @@
 import socket
 import threading
-import cv2
-import numpy as np
 from imageFunctionsMiddleware import *
-
+from imageProcessingModule import *
 
 def handle_client(client_socket, addr):
     print(f"Connection from: {addr}")
@@ -20,44 +18,15 @@ def handle_client(client_socket, addr):
                 
                 if image_bytes is not None:
                     if message == "gr":
-                        # display_image_from_bytes(image_bytes)
-                        # send_image_knownbytes(client_socket,(image_bytes,length))
-                        # Perform image processing
-                        image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
-                        # Example: Convert image to grayscale
-                        processed_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                        # Optionally, send the processed image back to the client
-                        # convert processed image back to bytes
-                        processed_image_bytes = cv2.imencode('.jpg', processed_image)[1].tobytes()
+                        processed_image_bytes = greyFilter(image_bytes)
                         # send processed image
                         send_image_knownbytes(client_socket, (processed_image_bytes, len(processed_image_bytes)))
                     elif message == "ed":
-                        image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
-                        # Convert image to grayscale
-                        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                        # Perform Canny edge detection
-                        edges = cv2.Canny(gray_image, 100, 200)  # Adjust the threshold values as needed
-                        # Display the detected edges
-                        # Optionally, send the detected edges back to the client
-                        # Convert edges to bytes
-                        edges_bytes = cv2.imencode('.jpg', edges)[1].tobytes()
+                        edges_bytes = edgeDetection(image_bytes)
                         # Send detected edges
                         send_image_knownbytes(client_socket, (edges_bytes, len(edges_bytes)))
                     elif message == "fl":
-                        # Perform image filtering
-                        image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
-                        
-                        # Apply Gaussian blur
-                        blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
-                        
-                        # Apply sharpening filter
-                        kernel_sharpening = np.array([[-1, -1, -1],
-                                                    [-1, 9, -1],
-                                                    [-1, -1, -1]])
-                        sharpened_image = cv2.filter2D(blurred_image, -1, kernel_sharpening)
-                        # Optionally, send the filtered image back to the client
-                        # Convert filtered image to bytes
-                        filtered_image_bytes = cv2.imencode('.jpg', sharpened_image)[1].tobytes()
+                        filtered_image_bytes = imageFiltering(image_bytes)
                         # Send filtered image
                         send_image_knownbytes(client_socket, (filtered_image_bytes, len(filtered_image_bytes)))
                     else:
